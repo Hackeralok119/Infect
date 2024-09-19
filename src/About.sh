718f3c79e774b379b7f8e4bcd2510fd02ace12cd
Hackeralok119 <70144305+Hackeralok119@users.noreply.github.com>
2024-09-19 12:55:01 +0530

    Add files via upload

---
+++ src/About.sh
@@ -0,0 +1,17 @@
+clear
+echo '
+    ┌──────────────────────────────────────────────────────────────┐
+    │  	               ┏━┓┏┓ ┏━┓╻ ╻╺┳╸	              │
+    │  		       ┣━┫┣┻┓┃ ┃┃ ┃ ┃ 		      │
+    │  		       ╹ ╹┗━┛┗━┛┗━┛ ╹ 		      │
+    │──────────────────────────────────────────────────────────────│
+    │                                                              │
+    │ >>   Hi, here you can see my telegram and github there       |
+    |       will be questions you will write in telegram.          │
+    │                                                              │
+    │ >>  Author   :  Hackeralok2.0                                │
+    │     Github   :  https://github.com/Hackeralok119               │
+    │     Telegram :  https://telegram.me/HackerAlokId / https://telegram.me/Grayhacker_Bot              │
+    │                                                              │
+    └──────────────────────────────────────────────────────────────┘ 
+'|lolcat -a -d 10 -p 1.5 -s 40.0
